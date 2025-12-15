fc867cc85d52b617b0b4ae668fcdc3725a34ecdb
kbh_kosta <galaxysgoo418@gmail.com>
2025-12-15 15:40:40 +0900

    refactor: index, main

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Plantory from './Plantory'
 import './index.css'
-<<<<<<< HEAD
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-=======
 import './css/layout.css'
 import './css/header.css'
 import './css/modal.css'
@@ -22,5 +14,4 @@ createRoot(document.getElementById('root')!).render(
       <Plantory />
     </BrowserRouter>
   </StrictMode>
->>>>>>> 5746b746d172a65e0ee83d20985b2658b7c0a98a
 )
