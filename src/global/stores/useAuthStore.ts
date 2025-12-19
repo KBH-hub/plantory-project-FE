50789047cbd0ccf0b2bf808414d7c76b950f922a
김재욱 <soo8848@naver.com>
2025-12-19 10:08:41 +0900

    fix: update jjwt

--- src/global/stores/useAuthStore.ts
+++ src/global/stores/useAuthStore.ts
@@ -1,49 +1,73 @@
 import { create } from "zustand";
+export type Role = 'USER' | 'ADMIN';
 
-interface User {
-  memberId: number;
-  membername: string;
-  nicknamee: string;
-  role: string;
+export interface AuthUser {
+    memberId: number;
+    membername: string;
+    role: Role;
 }
+
+export interface User extends AuthUser {
+    nickname: string;
+    phone: string;
+    address: string;
+    sharingRate: number | null;
+    skillRate: number | null;
+    managementRate: number | null;
+    stopDay: string | null;
+}
+
 interface AuthState {
     isLogin: boolean;
+    authUser: AuthUser | null;
     user: User | null;
     accessToken: string | null;
     initialized: boolean;
+
     login: (payload: {
-        user: User;
+        authUser: AuthUser;
         accessToken: string;
     }) => void;
+
+    setUser: (user: User) => void;
     setAccessToken: (token: string) => void;
     logout: () => void;
     setInitialized: () => void;
 }
 
+
 export const useAuthStore = create<AuthState>((set) => ({
     isLogin: false,
+    authUser: null,
     user: null,
     accessToken: null,
     initialized: false,
 
-    login: ({ user, accessToken }) =>
+    login: ({ authUser, accessToken }) =>
         set({
             isLogin: true,
-            user,
+            authUser,
             accessToken,
             initialized: true,
         }),
 
+    setUser: (user) =>
+        set((state) => ({
+            ...state,
+            user,
+            isLogin: true,
+        })),
+
     setAccessToken: (token) =>
         set((state) => ({
             ...state,
-            isLogin: true,
             accessToken: token,
         })),
 
     logout: () =>
         set({
             isLogin: false,
+            authUser: null,
             user: null,
             accessToken: null,
             initialized: true,
@@ -52,6 +76,7 @@ export const useAuthStore = create<AuthState>((set) => ({
     setInitialized: () =>
         set({ initialized: true }),
 }));
+
 
 /**
  * 개발용 디버깅 배포 시 지우기.
