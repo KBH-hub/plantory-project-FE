364fe6d40894db5d72512e2cd42cdc9428c811f8
kbh_kosta <galaxysgoo418@gmail.com>
2025-12-23 17:48:29 +0900

    refactor: update login error message

--- src/member/pages/Login.tsx
+++ src/member/pages/Login.tsx
@@ -44,8 +44,8 @@ export default function Login() {
             setAccessToken(data.accessToken);
 
             navigate("/", { replace: true });
-        } catch (err: any) {
-            setErrorMessage(err.response.data.message);
+        } catch {
+            setErrorMessage("아이디 또는 비밀번호가 올바르지 않습니다.");
         }
     };
 
