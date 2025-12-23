5e6cec25576d0bd9880543b75504ee7945922933
minse00 <tjdlsj@naver.com>
2025-12-24 00:01:28 +0900

    fix: fix type & alert error in question

--- src/community/question/services/questionListApi.ts
+++ src/community/question/services/questionListApi.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from "@/global/services/jjwt/axiosInstance"
 import type { QuestionListPageResponse } from "@/community/question/types/questionList"
 
-export const getQuestionList = async ( page: number, size: number, keyword?: string): Promise<QuestionListResponse> => {
+export const getQuestionList = async ( page: number, size: number, keyword?: string): Promise<QuestionListPageResponse> => {
   const res = await axiosInstance.get<QuestionListPageResponse>("/api/questions", {
     params: { page, size, keyword },
   });
